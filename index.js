4519e18f723c4e5d12fd5ce8ecb85acc614377ad
Eli Lustbader <elilustbader@fa44259d-b801-4d09-bc6f-54a5db4f6365.attlocal.net>
2025-07-10 16:28:54 -0400

    Fix invisible character at top of index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-cconst express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const WebSocket = require('ws');
 const http = require('http');
